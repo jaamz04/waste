935ee92cfb9c2157abaa66938ba156d8caa9aac3
jaamz04 <jamesarpilang04@gmail.com>
2025-04-30 20:24:47 +0800

    database table

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,24 @@
 const { required, number } = require('joi');
 const mongoose = require('mongoose');
 
+
+const userInfoSchema = new mongoose.Schema({
+    fullname: { type: String, required: true, trim: true },
+    address: { type: String, required: true },
+    contact: { type: String, required: true },
+    birthdate: { type: Date, required: true },
+    gender: { type: String, enum: ['Male', 'Female'], required: true },
+    assign_area: { type: String, required: true }
+});
+
+
+
 const userSchema = mongoose.Schema({
+    info: { type: mongoose.Schema.Types.ObjectId,
+         ref: 'UserInfo', 
+         required: true 
+        },
+
     name: {
         type: String,
         required: [true, "Name is required!"], 
@@ -25,6 +42,17 @@ const userSchema = mongoose.Schema({
         trim: true,
         select:false,
     },
+
+    u_role: { type: String, 
+        enum: ['admin', 'janitor', 'other'],
+         required: true 
+        },
+
+    status: { type: String, 
+        enum: ['active', 'inactive'], 
+        default: 'active' 
+    },
+
     verified:{
         type:Boolean,
         default:false,
@@ -50,4 +78,82 @@ const userSchema = mongoose.Schema({
     timestamps:true
 });
 
-module.exports = mongoose.model("User", userSchema);
+const sensoredDataSchema = new mongoose.Schema({
+    data_id: { type: String, 
+    required: true },
+    timestamp: Date
+});
+
+const activityLogSchema = new mongoose.Schema({
+    u_id: { type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true },
+    bin_id: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Bin',
+        required: true },
+        date: Date,
+        time: String,
+        status: String
+});
+
+const alertSchema = new mongoose.Schema({
+    sensored_id: { type: mongoose.Schema.Types.ObjectId, 
+    ref: 'SensoredData' },
+    bin_id: { type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Bin' },
+    status: String
+});
+
+const binSchema = new mongoose.Schema({
+    sensored_id: { type: mongoose.Schema.Types.ObjectId, 
+    ref: 'SensoredData' },
+    bin_code: { type: String,
+    required: true },
+    type: String,
+    location: String,
+    bin_level: Number,
+    capacity: Number,
+    last_collected: Date,
+    status: String
+});
+
+const historyLogSchema = new mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    time_in: Date,
+    time_out: Date,
+    date: Date
+});
+
+const notificationSchema = new mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    bin_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Bin' },
+    message: String,
+    notif_type: String,
+    created_at: Date,
+    send_time: Date,
+    status: String
+});
+
+
+
+const UserInfo = mongoose.model("UserInfo", userInfoSchema);
+const User = mongoose.model("User", userSchema);
+const SensoredData = mongoose.model("SensoredData", sensoredDataSchema);
+const ActivityLog = mongoose.model("ActivityLog", activityLogSchema);
+const Alert = mongoose.model("Alert", alertSchema);
+const Bin = mongoose.model("Bin", binSchema);
+const HistoryLog = mongoose.model("HistoryLog", historyLogSchema);
+const Notification = mongoose.model("Notification", notificationSchema);
+
+
+module.exports = {
+    User,
+    UserInfo,
+    SensoredData,
+    ActivityLog,
+    Alert,
+    Bin,
+    HistoryLog,
+    Notification
+};
